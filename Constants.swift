b91d4449a55cf4b3c802dfefe131bf254adc8426
Ashwin Mertes <ashwinmertes@ptr-4dz48kpk3r6bsnslig.18120a2.ip6.access.telenet.be>
2017-07-11 20:27:24 +0200

    Add beers and calculate BAC.

---
+++ Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Tipsy
+//
+//  Created by Ashwin Mertes on 10/07/2017.
+//  Copyright © 2017 Ashwin Mertes. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let volumeInCentiliters: Double = 25
+    static let abv = 0.052
+    static let alcoholInGrams: Double = 8
+    static let widmarkMale = 0.68
+    static let widmarkFemale = 0.55
+    static let oneKiloOfBlood = 1 / 1.055
+    static let betaFactor = 0.17
+}
--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bacLabel: UILabel!
+    
+    fileprivate var numberOfBeers = 0
 
+    @IBAction func addBeer(_ sender: UIButton) {
+        numberOfBeers += 1
+        
+        let bac = calculateBAC().roundTo(places: 5)
+        bacLabel.text = "Promille: \(bac)"
+    }
+    
+    func calculateBAC() -> Double {
+        let weight = 72.0 // variable
+        let differenceBetweenTwoDatesInHours = 0.0; // variable
+        
+        let nominator = Double(numberOfBeers) * Constants.volumeInCentiliters * Constants.abv * Constants.alcoholInGrams
+        let denominator = Constants.widmarkMale * weight * Constants.oneKiloOfBlood // for a male
+        let bac = (nominator / denominator) - differenceBetweenTwoDatesInHours * Constants.betaFactor
+        
+        return bac;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        
+        return (self * divisor).rounded() / divisor
+    }
 }
 
